3cca1184f83559b98bb91097d055fe30582f03c5
Noubar <noubar.kaloust@gmail.com>
2021-07-03 04:09:11 +0400

    handeled the checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters = false;
+  includeSymbols = false;
+  includeNumbers = false;
+  password = '';
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onButtonClick() {
+    console.log(this.includeLetters, this.includeNumbers, this.includeSymbols);
+    this.password = 'my password';
+  }
+  getPassword() {
+    return this.password;
+  }
 }
