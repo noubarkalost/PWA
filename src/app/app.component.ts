82ecefc1bc44eea1320e0acf2c252773af30baa3
Noubar <noubar.kaloust@gmail.com>
2021-07-04 18:34:07 +0400

    clean up and code refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,30 @@ export class AppComponent {
   includeLetters: boolean = false;
   includeSymbols: boolean = false;
   includeNumbers: boolean = false;
-  footerDate: any = new Date(Date.now()).getFullYear().toString();
+  footerDate: string = new Date(Date.now()).getFullYear().toString();
   password: string = '';
   isCopied: boolean = false;
-  onChangeLength(event: any) {
+
+  onChangeLength(event: any): void {
     const { value } = event.target;
     const parsedValue = parseInt(value);
     if (!isNaN(parsedValue)) {
       this.length = parsedValue;
-      console.log(parsedValue);
     }
   }
-  onChangeUseLetters() {
+  onChangeUseLetters(): void {
     this.includeLetters = !this.includeLetters;
   }
-  onChangeUseNumbers() {
+  onChangeUseNumbers(): void {
     this.includeNumbers = !this.includeNumbers;
   }
-  onChangeUseSymbols() {
+  onChangeUseSymbols(): void {
     this.includeSymbols = !this.includeSymbols;
   }
-  onButtonClick() {
-    const numbers = '1234567890';
-    const letters = 'abcdefghijklmnopqrstuvwxyz';
-    const symbols = '!@#$%^&*()';
+  onButtonClick(): void {
+    const numbers: string = '1234567890';
+    const letters: string = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols: string = '!@#$%^&*()';
     let valideChars = '';
     if (this.includeLetters) {
       valideChars += letters;
@@ -47,27 +47,24 @@ export class AppComponent {
     if (this.includeSymbols) {
       valideChars += symbols;
     }
-    let generatedPassword = '';
+    let generatedPassword: string = '';
     for (let i = 0; i < this.length; i++) {
-      const index = Math.floor(Math.random() * valideChars.length);
+      const index: number = Math.floor(Math.random() * valideChars.length);
       generatedPassword += valideChars[index];
     }
     this.password = generatedPassword;
   }
-  getPassword() {
+  getPassword(): string {
     return this.password;
   }
-  copyText() {
+  copyText(): void {
     this.clipboardApi.copyFromContent(this.password);
     this.isCopied = true;
-    console.log(this.isCopied);
     this.resetIsCopied();
-    console.log(this.isCopied);
   }
-  resetIsCopied() {
+  resetIsCopied(): void {
     setTimeout(() => {
       this.isCopied = false;
-      console.log(this.isCopied);
     }, 4000);
   }
 }
